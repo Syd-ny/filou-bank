80d7bf7e64d797f7b89bbf9316ed4263c66a3c22
Syd-ny <sydneymillon@gmail.com>
2023-12-19 17:02:11 +0100

    livretA assuranceVie

--- src/main/java/fr/epsi/b32324c2/entities/AssuranceVie.java
+++ src/main/java/fr/epsi/b32324c2/entities/AssuranceVie.java
@@ -1,11 +1,59 @@
 package fr.epsi.b32324c2.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.Column;
 import java.util.Date;
+import java.util.Objects;
 
 @Entity
 public class AssuranceVie extends Compte {
-    private Date dateFin;
-    private double taux;
-    // ... getters et setters ...
+
+    @Column(nullable = false)
+    private double tauxInteret;
+
+    @Column(nullable = false)
+    private Date dateExpiration;
+
+    public AssuranceVie() {
+        // Constructeur par défaut requis par JPA
+    }
+
+    // Getters et Setters
+
+    public double getTauxInteret() {
+        return tauxInteret;
+    }
+
+    public void setTauxInteret(double tauxInteret) {
+        this.tauxInteret = tauxInteret;
+    }
+
+    public Date getDateExpiration() {
+        return dateExpiration;
+    }
+
+    public void setDateExpiration(Date dateExpiration) {
+        this.dateExpiration = dateExpiration;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof AssuranceVie)) {
+            return false;
+        }
+        if (!super.equals(obj)) {
+            return false;
+        }
+        AssuranceVie that = (AssuranceVie) obj;
+        return Double.compare(that.tauxInteret, tauxInteret) == 0 &&
+                Objects.equals(dateExpiration, that.dateExpiration);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), tauxInteret, dateExpiration);
+    }
 }
--- src/main/java/fr/epsi/b32324c2/entities/LivretA.java
+++ src/main/java/fr/epsi/b32324c2/entities/LivretA.java
@@ -1,9 +1,46 @@
 package fr.epsi.b32324c2.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import java.util.Objects;
+
 
 @Entity
 public class LivretA extends Compte {
+
+    @Column(nullable = false)
     private double taux;
-    // ... getters et setters ...
+
+    public LivretA() {
+
+    }
+
+    // Getters et Setters
+
+    public double getTaux() {
+        return taux;
+    }
+
+    public void setTaux(double taux) {
+        this.taux = taux;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        LivretA livretA = (LivretA) obj;
+        return Double.compare(livretA.taux, taux) == 0;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(taux);
+    }
+
 }
